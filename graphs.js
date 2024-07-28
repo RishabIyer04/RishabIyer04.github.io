613809664e50f405031c0d770bedeb059d9becf2
Rishab Iyer <riyer20@illinois.edu>
2024-07-27 23:15:07 -0500

    Drop-down filter fixing; Color changing based on Team

--- graphs.js
+++ graphs.js
@@ -8,6 +8,40 @@ async function loadData() {
     return data;
 }
 
+const teamColors = {
+    "ATL": "#E03A3E", // Atlanta Hawks
+    "BOS": "#007A33", // Boston Celtics
+    "BKN": "#000000", // Brooklyn Nets
+    "CHA": "#1D1160", // Charlotte Hornets
+    "CHI": "#CE1141", // Chicago Bulls
+    "CLE": "#6F263D", // Cleveland Cavaliers
+    "DAL": "#00538C", // Dallas Mavericks
+    "DEN": "#0E2240", // Denver Nuggets
+    "DET": "#C8102E", // Detroit Pistons
+    "GSW": "#1D428A", // Golden State Warriors
+    "HOU": "#CE1141", // Houston Rockets
+    "IND": "#002D62", // Indiana Pacers
+    "LAC": "#C8102E", // Los Angeles Clippers
+    "LAL": "#552583", // Los Angeles Lakers
+    "MEM": "#5D76A9", // Memphis Grizzlies
+    "MIA": "#98002E", // Miami Heat
+    "MIL": "#00471B", // Milwaukee Bucks
+    "MIN": "#0C2340", // Minnesota Timberwolves
+    "NOP": "#0C2340", // New Orleans Pelicans
+    "NYK": "#006BB6", // New York Knicks
+    "OKC": "#007AC1", // Oklahoma City Thunder
+    "ORL": "#0077C0", // Orlando Magic
+    "PHI": "#006BB6", // Philadelphia 76ers
+    "PHX": "#1D1160", // Phoenix Suns
+    "POR": "#E03A3E", // Portland Trail Blazers
+    "SAC": "#5A2D81", // Sacramento Kings
+    "SAS": "#C4CED4", // San Antonio Spurs
+    "TOR": "#CE1141", // Toronto Raptors
+    "UTA": "#002B5C", // Utah Jazz
+    "WAS": "#002B5C"  // Washington Wizards
+};
+
+
 function drawPlot(svgId, data, position) {
     const sortedData = data.filter(d => d.Pos === position);
 
@@ -38,7 +72,7 @@ function drawPlot(svgId, data, position) {
         .attr('cx', d => x(d.Age))
         .attr('cy', d => y(d.PTS))
         .attr('r', 7)
-        .style("fill", "#69b3a2")
+        .style("fill", d => teamColors[d.Tm])
         .style("stroke", "black");
 
     // Add x-axis
